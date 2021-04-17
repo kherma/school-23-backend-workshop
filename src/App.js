6fec7745f760312e3c285a38ae3daca71b03a202
kherma <kamil.herma01@gmail.com>
2021-04-17 17:32:28 +0200

    Initial react router config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
-export default App;
+import { MainLayout } from './components/layouts/MainLayout/MainLayout';
+import { Homepage } from './components/views/Homepage/Homepage';
+import { Post } from './components/views/Post/Post';
+import { PostEdit } from './components/views/PostEdit/PostEdit';
+import { PostAdd } from './components/views/PostAdd/PostAdd';
+import { NotFound } from './components/views/NotFound/NotFound';
+
+const App = () => {
+  return (
+    // <Provider store={store}>
+    <BrowserRouter>
+      <MainLayout>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/post/add" component={PostAdd} />
+          <Route exact path="/post/:id" component={Post} />
+          <Route exact path="/post/:id/edit" component={PostEdit} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
+    // </Provider>
+  );
+};
+
+export { App };
