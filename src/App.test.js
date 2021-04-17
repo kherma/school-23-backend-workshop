e014adc1884b50a0cc8a0994c5e1a711e15c1510
kherma <kamil.herma01@gmail.com>
2021-04-17 16:02:26 +0200

    Initial eslint and prettier config

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import App from "./App";
-import { shallow } from "enzyme";
+import React from 'react';
+import App from './App';
+import { shallow } from 'enzyme';
 
-describe("Component App", () => {
-  it("Should render without crashing", () => {
+describe('Component App', () => {
+  it('Should render without crashing', () => {
     const component = shallow(<App />);
     expect(component).toBeTruthy();
   });
